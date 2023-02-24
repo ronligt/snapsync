253a4592c31522df40af59cec2d664d113042381
Ronald Ligteringen <r.ligteringen@tudelft.nl>
2023-02-24 10:39:09 +0100

    added hourly backup

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,7 @@
 DOW="Friday"  # which weekday must be kept
 MAXWEEK=52    # how many weekdays must be kept
 MAXDAY=31     # how many days must be kept
+MAXHOUR=1     # how many hours must be kept: MAXHOUR*24 hours
 
 # first backup ever
 first=`ls -td 2* | tail -1`
@@ -49,7 +50,32 @@ while [ "$cont" = "1" ]; do
 done
 
 # DAYS, keep last $MAXDAY days
-findstr=$(echo $findstr " ! -mtime -$MAXDAY")
+nowstr=`date +"%Y%m%d"`
+i=0
+cont=1
+nodays=0
+while [ "$cont" = "1" ]; do
+  datestr=`date --date "now -$i day" +"%Y%m%d"`
+  if [ "$datestr" -ge "${first:0:8}" ]; then
+    if [ "$datestr" -le "$nowstr" ]; then
+      day=`find . -maxdepth 1 -mindepth 1 -name "$datestr*" -type d -printf "%f\n" | sort | head -1`
+      nodays=$(echo "$nodays + 1" | bc)
+      if [ "$nodays" = "$MAXDAY" ]; then
+        cont=0
+      fi
+      if [ "$day" != "" ]; then
+        findstr=$(echo $findstr " ! -name $day")
+      fi
+    fi
+    i=$(echo "$i + 1" | bc)
+  else
+    cont=0
+  fi
+done
 
-find . -maxdepth 1 -mindepth 1 -type d $findstr -printf "%f\n" -exec rm -rf {} \;
-#find . -maxdepth 1 -mindepth 1 -type d $findstr -printf "%f\n"
+# HOURS, keep last $MAXHOUR*24 hours
+findstr=$(echo $findstr " ! -mtime -$MAXHOUR")
+
+#find . -maxdepth 1 -mindepth 1 -type d $findstr -printf "%f\n" -exec rm -rf {} \;
+echo $findstr
+find . -maxdepth 1 -mindepth 1 -type d $findstr -printf "%f\n"
