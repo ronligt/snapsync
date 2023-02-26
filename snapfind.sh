a195a340285bf5e83d604a33c57c095f23a7ac47
Ronald Ligteringen <r.ligteringen@gmail.com>
2023-02-26 16:25:57 +0100

    test

--- snapfind.sh
+++ snapfind.sh
@@ -6,6 +6,7 @@ paths=$(locate --database /data/snapsync/home/mlocate.db $1 | sort | xargs ls -i
 prev_inode=0
 for path in $paths
 do
+    echo $path
     snapdate=$(echo $path|cut -f5 -d'/')
     inode=$(echo $path|cut -f1 -d' ')
     snappath=$(echo $path|cut -f6- -d'/')
@@ -15,7 +16,7 @@ do
         then
             echo "    identical from " $first_snapdate to $prev_snapdate
         fi
-        echo $path "first occurence"
+        echo $snappath "first occurence"
         first_snapdate=$snapdate
         prev_inode=$inode
     else
