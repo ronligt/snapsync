0bee6234ed1b3adaa88bc613879350207a53c65e
Ronald Ligteringen <r.ligteringen@gmail.com>
2023-02-26 15:21:47 +0100

    echo

--- snapfind.sh
+++ snapfind.sh
@@ -4,6 +4,6 @@ paths=$(locate --database /data/snapsync/home/mlocate.db $1 | sort)
 
 for path in $paths
 do
-    snapdate=$(cut -f5 -d'/')
+    snapdate=$(echo $path|cut -f5 -d'/')
     echo $snapdate
 done
